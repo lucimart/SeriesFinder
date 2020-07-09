ad0447f445bbe78215e88c8336c02fe713f1f70f
Lucía Martínez <lucimart@student.42madrid.com>
2020-07-09 19:46:23 +0200

    API functionality and more HTML/CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,47 @@
-'use strict';
+"use strict";
+// vars
+const inputElem = document.querySelector(".js-input");
+const searchButtonElem = document.querySelector(".js-search");
+const listElem = document.createElement("ul");
+const smallElem = document.querySelector(".js-small");
+const mainElem = document.querySelector(".js-main");
 
-console.log('>> Ready :)');
+function searchHandler(event) {
+  event.preventDefault();
+  if (!listElem.parentElement) mainElem.appendChild(listElem);
+  listElem.innerHTML = "";
+  smallElem.classList.add("hidden");
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputElem.value}`)
+    .then((response) => response.json())
+    .then((data) => paintSeries(data));
+  return false;
+}
+
+function paintSeries(arr) {
+  let elem;
+  let serieNameElem;
+  let serieImgElem;
+  if (arr.length === 0) {
+    smallElem.classList.toggle("hidden");
+    smallElem.innerHTML = "There were no results for your search.";
+    listElem.remove();
+  }
+  for (const serie of arr) {
+    elem = document.createElement("li");
+    elem.setAttribute("data-id", `${serie.show.id}`);
+    serieNameElem = document.createElement("p");
+    serieNameElem.appendChild(document.createTextNode(`${serie.show.name}`));
+    serieImgElem = document.createElement("img");
+    if (serie.show.image)
+      serieImgElem.setAttribute("src", `${serie.show.image.medium}`);
+    else
+      serieImgElem.src = `https://via.placeholder.com/210x295/ffffff/666666/?text=TV`;
+    elem.appendChild(serieNameElem);
+    elem.appendChild(serieImgElem);
+    listElem.appendChild(elem);
+  }
+}
+
+//Event Listeners
+searchButtonElem.addEventListener("click", searchHandler);
+inputElem.addEventListener("submit", searchHandler);
