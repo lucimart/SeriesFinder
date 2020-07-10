7fdffb01d98b8804ef078b152fa41f1a83b220ff
Lucía Martínez <lucimart@student.42madrid.com>
2020-07-10 09:11:36 +0200

    finished fav, remove, remove all

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,22 @@
 "use strict";
 
-// vars
+// Query Selectors
 const inputElem = document.querySelector(".js-input");
 const searchButtonElem = document.querySelector(".js-search");
-const listSeriesElem = document.createElement("ul");
-listSeriesElem.classList.add("js-listSeries");
 const smallElem = document.querySelector(".js-small");
 const mainElem = document.querySelector(".js-main");
 const favButton = document.querySelector(".js-favButton");
 const favPanel = document.querySelector(".js-favPanel");
-// const transparentPanel = document.querySelector(".js-transparentPanel");
 const favList = document.querySelector(".js-favList");
+const clearAllButton = document.querySelector(".js-clearAll");
 
+// vars
 let favs = [];
 
+//script scope created elements
+const listSeriesElem = document.createElement("ul");
+listSeriesElem.classList.add("js-listSeries");
+//This function searches on the API whatever is in the input and calls paintSeries()
 function searchHandler(event) {
   if (localStorage.getItem("favIdArr"))
     favs = JSON.parse(localStorage.getItem("favIdArr"));
@@ -26,17 +29,22 @@ function searchHandler(event) {
     .then((data) => paintSeries(data, false));
   return false;
 }
-
+//This function accepts whether the search handler list or the fav list and handles the macro
+//scope of the painting
 function paintSeries(arr, isFavList) {
   if (!isFavList && arr.length === 0) {
     smallElem.classList.toggle("hidden");
     smallElem.innerHTML = "There were no results for your search.";
     listSeriesElem.remove();
+  } else if (isFavList && arr.length === 0) {
+    favList.innerHTML = `<p>You haven't faved any series.<br><br>Please search for some series in the search bar and click on it to fav it.</p>`;
+    return;
   }
   if (isFavList) favList.innerHTML = "";
   for (const serie of arr) appendSerieToSerieList(serie, isFavList);
 }
-
+//This is the function that actually adds the serie element to the normal/fav list by constructing
+// the element, adding its corresponding properties
 function appendSerieToSerieList(serie, isFavList) {
   let elem;
   let serieNameElem;
@@ -44,6 +52,7 @@ function appendSerieToSerieList(serie, isFavList) {
   let id;
   let name;
   let img;
+  elem = document.createElement("li");
   if (!isFavList) {
     id = serie.show.id;
     name = serie.show.name;
@@ -52,8 +61,12 @@ function appendSerieToSerieList(serie, isFavList) {
     id = serie.id;
     name = serie.name;
     img = serie.img;
+    let favRemoveButton = document.createElement("button");
+    favRemoveButton.classList.add("favRemove");
+    favRemoveButton.innerHTML = "X";
+    elem.appendChild(favRemoveButton);
+    favRemoveButton.addEventListener("click", favRemoveHandler);
   }
-  elem = document.createElement("li");
   elem.setAttribute("data-id", `${id}`);
   serieNameElem = document.createElement("p");
   serieNameElem.appendChild(document.createTextNode(`${name}`));
@@ -68,7 +81,13 @@ function appendSerieToSerieList(serie, isFavList) {
   if (!isFavList) listSeriesElem.appendChild(elem);
   else favList.appendChild(elem);
 }
-
+//This function returns true if the serie is in the favs array, false otherwise
+function isSerieFaved(serie, favArr) {
+  for (const elem of favArr) if (elem.id === serie.dataset.id) return true;
+  return false;
+}
+//This function handles the click on the serie element to add it to the favs,
+//update the localStorage and paint the favs in its list
 function favHandler(event) {
   let serie;
   if (event.target.nodeName === "LI") serie = event.target;
@@ -89,31 +108,44 @@ function favHandler(event) {
   localStorage.setItem("favIdArr", JSON.stringify(favs));
   paintSeries(favs, true);
 }
-function isSerieFaved(serie, favArr) {
-  for (const elem of favArr) if (elem.id === serie.dataset.id) return true;
-  return false;
-}
+//This function returns the index of the serie in the favs array, -1 if serie not found
 function indexOfSerieInFav(serie, favArr) {
-  for (const index in favArr)
-    if (favArr[index] && favArr[index].id === serie.dataset.id) return index;
+  for (let i = 0; i < favArr.length; i++)
+    if (favArr[i].id === serie.dataset.id) return i;
   return -1;
 }
-
+//This function toggles different classes corresponding the fav button interaction in the header
 function favButtonHandler() {
   favButton.classList.toggle("rotFav");
   favPanel.classList.toggle("hidden");
-  //   transparentPanel.classList.toggle("hidden");
 }
-
+//This function handles the event of clicking a remove button in the fav list to remove one serie
+function favRemoveHandler(event) {
+  let id = event.currentTarget.parentNode.dataset.id;
+  for (let i = 0; i < favs.length; i++)
+    if (favs[i].id === id) favs.splice(i, 1);
+  localStorage.setItem("favIdArr", JSON.stringify(favs));
+  paintSeries(favs, true);
+  for (const liElem of Array.from(listSeriesElem.childNodes))
+    if (liElem.dataset.id === id) liElem.classList.remove("fav");
+}
+//This function handles the event of clicking remove all button, which clears the fav list
+function favRemoveAllHandler() {
+  favs = [];
+  localStorage.setItem("favIdArr", JSON.stringify(favs));
+  paintSeries(favs, true);
+  for (const liElem of Array.from(listSeriesElem.childNodes))
+    liElem.classList.remove("fav");
+}
+//This function is onLoad event to <body>, it gets the localStorage favs arr and paints the favs
 function init() {
   if (localStorage.getItem("favIdArr"))
     favs = JSON.parse(localStorage.getItem("favIdArr"));
   if (favs) paintSeries(favs, true);
 }
-
 //Event Listeners
 searchButtonElem.addEventListener("click", searchHandler);
 inputElem.addEventListener("submit", searchHandler);
 listSeriesElem.addEventListener("click", favHandler);
 favButton.addEventListener("click", favButtonHandler);
-// transparentPanel.addEventListener("click", () => favButton.click());
+clearAllButton.addEventListener("click", favRemoveAllHandler);
