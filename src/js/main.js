7a625d44dc38e1ae490a523c22cdcc472b2fc81d
Lucía Martínez <lucimart@student.42madrid.com>
2020-07-10 11:41:26 +0200

    unselect, stars, sky

--- src/js/main.js
+++ src/js/main.js
@@ -17,8 +17,10 @@ let favs = [];
 const listSeriesElem = document.createElement("ul");
 listSeriesElem.classList.add("js-listSeries");
 
-//bonus clouds
-const cloudsElem = document.querySelector(".clouds");
+//bonus clouds & stars & sky
+const cloudsElem = document.querySelector(".js-clouds");
+const starsElem = document.querySelector(".js-stars");
+const skyElem = document.querySelector(".js-sky");
 //This function searches on the API whatever is in the input and calls paintSeries()
 function searchHandler(event) {
   if (localStorage.getItem("favIdArr"))
@@ -143,18 +145,26 @@ function favRemoveAllHandler() {
     liElem.classList.remove("fav");
 }
 //BONUS CLOUD FUNCTIONS
+//Utility function, generates a random floating number between start and end
 function randomInRangeFloat(start, end) {
   return Math.random() * (end - start) + start;
 }
+//Utility function, generates a random integer number between start and end
 function randomInRangeInt(start, end) {
   return Math.floor(Math.random() * (end - start + 1)) + start;
 }
+//Utility function, returns the pixel equivalent of the vw introduced
 function vwToPx(vwVal) {
   return (vwVal * document.documentElement.clientWidth) / 100;
 }
+//Utility function, returns the vw equivalent of the pixel introduced
 function pxToVw(pxVal) {
   return pxVal * (100 / document.documentElement.clientWidth);
 }
+//This function given below parameters, generates a random number of clouds
+//between minClouds and maxClouds, of sized scaling from minCloudSize to
+//maxCloudSize based on it's CSS width, then it randomly places it on the page
+//and gives it a data-speed which is inversely proportional to its scale.
 function cloudsIniPos(isGen) {
   const minClouds = 4;
   const maxClouds = 8;
@@ -189,6 +199,7 @@ function cloudsIniPos(isGen) {
     cloud.dataset.speed = 1 + 1 / scaleVal;
   }
 }
+//This function handles the movement of the cloud by altering its left style using the data-speed
 function cloudMoveHandler() {
   const speedBase = 5;
   for (const cloud of cloudsElem.childNodes) {
@@ -202,20 +213,132 @@ function cloudMoveHandler() {
     }
   }
 }
+//Move the clouds every X ms
 setInterval(cloudMoveHandler, 2000);
 //END BONUS CLOUD
+//BONUS STARS START
+//generate between minStars and maxStars amount of stars, assign a type of .svg, a random
+//opacity val between opacMin and opacMax and randomly place in on the page
+function starsIniPos() {
+  const minStars = 50;
+  const maxStars = 80;
+  const opacMin = 0.05;
+  const opacMax = 0.35;
+  for (let i = 0; i < randomInRangeInt(minStars, maxStars); i++) {
+    const starType = randomInRangeInt(1, 4);
+    const starEl = document.createElement("object");
+    starEl.setAttribute("type", "image/svg+xml");
+    starEl.setAttribute("data", `./assets/images/star${starType}.svg`);
+    starEl.classList.add("js-star", "star", `star${starType}`);
+    starsElem.appendChild(starEl);
+  }
+  for (const star of starsElem.childNodes) {
+    const opacityVal = randomInRangeFloat(opacMin, opacMax);
+    star.style.top = `${randomInRangeInt(-5, 105)}vh`;
+    star.style.left = `${randomInRangeInt(-5, 105)}vw`;
+    star.style.opacity = `${opacityVal}`;
+  }
+}
+//this function handles the animation of each star, giving it a chance
+//of liting of chanceOfAnimating (ex: 0.1 = 10%) and proceeds to delete
+//the animation once its over, so that it could be readded in the future
+function starLitHandler() {
+  const chanceOfAnimating = 0.1;
+  for (const star of starsElem.childNodes) {
+    if (randomInRangeFloat(0, 1) >= 1 - chanceOfAnimating) {
+      star.classList.add("starAnimation");
+      star.addEventListener("webkitAnimationEnd", (ev) =>
+        ev.currentTarget.classList.remove("starAnimation")
+      );
+      star.addEventListener("animationend", (ev) =>
+        ev.currentTarget.classList.remove("starAnimation")
+      );
+    }
+  }
+}
+//Lit stars every x ms
+setInterval(starLitHandler, 5000);
+//Bonus STARS END
+//BONUS Stepped background gradient START
+function hex(c) {
+  var s = "0123456789abcdef";
+  var i = parseInt(c);
+  if (i == 0 || isNaN(c)) return "00";
+  i = Math.round(Math.min(Math.max(0, i), 255));
+  return s.charAt((i - (i % 16)) / 16) + s.charAt(i % 16);
+}
+/* Convert an RGB triplet to a hex string */
+function convertToHex(rgb) {
+  return hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);
+}
+/* Remove '#' in color hex string */
+function trim(s) {
+  return s.charAt(0) == "#" ? s.substring(1, 7) : s;
+}
+/* Convert a hex string to an RGB triplet */
+function convertToRGB(hex) {
+  var color = [];
+  color[0] = parseInt(trim(hex).substring(0, 2), 16);
+  color[1] = parseInt(trim(hex).substring(2, 4), 16);
+  color[2] = parseInt(trim(hex).substring(4, 6), 16);
+  return color;
+}
+function generateColor(colorStart, colorEnd, colorCount) {
+  // The beginning of your gradient
+  var start = convertToRGB(colorStart);
+
+  // The end of your gradient
+  var end = convertToRGB(colorEnd);
+
+  // The number of colors to compute
+  var len = colorCount;
+
+  //Alpha blending amount
+  var alpha = 0.0;
+
+  var saida = [];
+  saida.push(trim(colorEnd));
+  for (let i = 0; i < len; i++) {
+    var c = [];
+    alpha += 1.0 / len;
+
+    c[0] = start[0] * alpha + (1 - alpha) * end[0];
+    c[1] = start[1] * alpha + (1 - alpha) * end[1];
+    c[2] = start[2] * alpha + (1 - alpha) * end[2];
+
+    saida.push(convertToHex(c));
+  }
+
+  return saida;
+}
+function genSky() {
+  //Color Gradient generated by https://stackoverflow.com/a/32257791
+  //Using generateColor("#4c566a", "#2e3440", 30)
+  // $$color-polarNight-darkest to $color-polarNight-brightest
+  const gradient = generateColor("#4c566a", "#2e3440", 25);
+  for (let i = 0; i < gradient.length; i++) {
+    const skyStripe = document.createElement("div");
+    skyStripe.classList.add("stripe");
+    skyStripe.style.backgroundColor = `#${gradient[i]}`;
+    skyStripe.style.height = `${100 / gradient.length}vh`;
+    skyElem.appendChild(skyStripe);
+  }
+}
+//BONUS Stepped background gradient END
 //This function is onLoad event to <body>, it gets the localStorage favs arr and paints the favs
 function init() {
   if (localStorage.getItem("favIdArr"))
     favs = JSON.parse(localStorage.getItem("favIdArr"));
   if (favs) paintSeries(favs, true);
+  genSky();
   cloudsIniPos(false);
+  starsIniPos();
 }
 //This function generates clouds and animates them constantly through the site
 //Event Listeners
 searchButtonElem.addEventListener("click", searchHandler);
 inputElem.addEventListener("submit", searchHandler);
-// inputElem.addEventListener("keyup", searchHandler);
+inputElem.addEventListener("keyup", searchHandler);
 listSeriesElem.addEventListener("click", favHandler);
 favButton.addEventListener("click", favButtonHandler);
 clearAllButton.addEventListener("click", favRemoveAllHandler);
